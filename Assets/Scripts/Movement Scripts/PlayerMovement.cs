3ceec78ccee36e85002f191a21cc18f26a3acfd5
Arshnoor Singh <arshnoor88@gmail.com>
2024-08-09 15:58:23 +0530

    Player movement and Cinemachine

--- Assets/Scripts/Movement Scripts/PlayerMovement.cs
+++ Assets/Scripts/Movement Scripts/PlayerMovement.cs
@@ -1,26 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector2 movementValues = Vector2.zero;
+    public float frameDistance = 100f;
+
     public void IAAccelerate(InputAction.CallbackContext context)
     {
-        Vector2 moveValues = context.ReadValue<Vector2>();
-
-        transform.Translate(moveValues.x, 0, moveValues.y);
+        movementValues = context.ReadValue<Vector2>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(movementValues.x * frameDistance * Time.deltaTime, 0, movementValues.y * frameDistance * Time.deltaTime);
     }
 }
