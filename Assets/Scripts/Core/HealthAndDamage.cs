4b5e80a889829715e5206cd13f6fbecba4b1d979
Arshnoor Singh <arshnoor88@gmail.com>
2024-09-06 03:32:02 -0700

    Intro to UI and Delegates

--- Assets/Scripts/Core/HealthAndDamage.cs
+++ Assets/Scripts/Core/HealthAndDamage.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class HealthAndDamage : MonoBehaviour
 {
+    public float maxHealth = 100f;
     public float health = 100f;
     public float damage = 10f;
+
+    delegate void MoreDrama();
+    delegate void DamageTakenDelegate(float drama);
+
+    DamageTakenDelegate delegate_DamageTaken;
+    MoreDrama Monkey;
+
+    private void Start()
+    {
+        delegate_DamageTaken = AcceptDamage;
+        Monkey = Death;
+    }
 
     public void AcceptDamage(float incomingDamage)
     {
@@ -15,9 +28,21 @@ public class HealthAndDamage : MonoBehaviour
         }
     }
 
+    private void SomeRandomFunction()
+    {
+
+    }
+
     public void Death()
     {
         Debug.Log("Health less than 0. Destroying Game Object");
         Destroy(gameObject);
     }
+
+    public float GetHealthRatio()
+    {
+        return health / maxHealth;
+    }
+
+
 }
---
+++ Assets/Scripts/Player Scripts/PlayerUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    public Image healthBar;
+
+    private void Start()
+    {
+        healthBar.fillAmount = GetComponent<HealthAndDamage>().GetHealthRatio();
+    }
+
+    public void UpdateHealthBar(float newHealthRatio)
+    {
+
+    }
+
+    public void NormalFunction()
+    {
+
+    }
+}
